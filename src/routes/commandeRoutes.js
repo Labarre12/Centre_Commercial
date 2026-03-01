818f0919e3e9f87f5101a0502b1ceb7f57cf0388
HanaMendrika <mendrikaandriamarolafy@gmail.com>
2026-03-01 22:24:45 +0300

    Update commandeRoutes.js

--- src/routes/commandeRoutes.js
+++ src/routes/commandeRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const commandeController = require('../controllers/CommandeController');
 
+// Commander et payer en premier pour éviter conflits
+router.post('/boutique/:idBoutique/commander-payer', commandeController.commanderEtPayer);
+
+// Routes classiques commandes
 router.get('/:idCommande', commandeController.getCommandeById);
 router.put('/:idCommande', commandeController.updateCommande);
 router.delete('/:idCommande', commandeController.deleteCommande);
-
-//commande et payer
-router.post("/commander-payer/:idBoutique", commandeController.commanderEtPayer);
 
 module.exports = router;
