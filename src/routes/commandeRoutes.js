a5697d29b8df7a01a475f06a985179d0aa9ff8ee
HanaMendrika <mendrikaandriamarolafy@gmail.com>
2026-03-01 22:14:29 +0300

    Update commandeRoutes.js

--- src/routes/commandeRoutes.js
+++ src/routes/commandeRoutes.js
@@ -7,6 +7,6 @@ router.put('/:idCommande', commandeController.updateCommande);
 router.delete('/:idCommande', commandeController.deleteCommande);
 
 //commande et payer
-router.post("/:idBoutique/commander-payer", controller.commanderEtPayer);
+router.post("/:idBoutique/commander-payer", commandeController.commanderEtPayer);
 
 module.exports = router;
