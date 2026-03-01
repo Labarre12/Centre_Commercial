9ce8aed180320f3e8c7483c93bb80017ac709ff0
HanaMendrika <mendrikaandriamarolafy@gmail.com>
2026-03-01 22:04:05 +0300

    Changed route

--- src/routes/client/clientRoutes.js
+++ src/routes/client/clientRoutes.js
@@ -38,7 +38,6 @@ router.get('/clients/:idClient', profilController.getProfilClient);
 router.put('/clients/:idClient', profilController.updateProfilClient);
 router.delete('/clients/:idClient', profilController.deleteProfilClient);
 
-//commande et payer
-router.post("/commandes/:idBoutique/commander-payer", controller.commanderEtPayer);
+
 
 module.exports = router;
--- src/routes/commandeRoutes.js
+++ src/routes/commandeRoutes.js
@@ -6,4 +6,7 @@ router.get('/:idCommande', commandeController.getCommandeById);
 router.put('/:idCommande', commandeController.updateCommande);
 router.delete('/:idCommande', commandeController.deleteCommande);
 
+//commande et payer
+router.post("/commandes/:idBoutique/commander-payer", controller.commanderEtPayer);
+
 module.exports = router;
