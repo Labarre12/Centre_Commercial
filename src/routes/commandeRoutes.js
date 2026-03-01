e89e25bfccb719d5eeb5e26bfb02eb35ed0be0bc
HanaMendrika <mendrikaandriamarolafy@gmail.com>
2026-03-01 22:34:58 +0300

    test routes

--- src/routes/commandeRoutes.js
+++ src/routes/commandeRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const commandeController = require('../controllers/CommandeController');
 
+// Route de test pour vérifier que le routeur fonctionne
+router.get('/test', (req, res) => {
+  res.json({ message: 'Route commande fonctionne' });
+});
+
 // Commander et payer en premier pour éviter conflits
 router.post('/boutique/:idBoutique/commander-payer', commandeController.commanderEtPayer);
 
